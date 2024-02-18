dd3e0b3ca5bb6a676455633de5e152abf74f4d82
omer589 <omerr589@gmail.com>
2024-02-18 21:14:46 +0300

    Reference Tpyes

--- ReferenceTypes/Program.cs
+++ ReferenceTypes/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using ReferenceTypes;
+
 int sayi1 = 10;
 int sayi2 = 20;
 
@@ -19,3 +21,32 @@ sayilar2[0] = 100;
 
 Console.WriteLine(sayilar1[0]); // Diziler referans tipler olduğundan dolayı sonuç 100
                                 // arrays, class, interfaces referance tiplerdir
+
+
+Person person1 = new Person();
+Person person2 = new Person();
+
+person1.FirstName = "Ömer";
+person2 = person1;
+
+person1.FirstName = "Engin";
+
+Console.WriteLine(person2.FirstName); // Engin
+
+Customer customer = new Customer();
+Employee employee = new Employee();
+
+customer.FirstName = "Salih";
+employee.FirstName = "Fatih";
+customer.CreditCardNumber = "1231546";
+
+Person person3 = customer;
+Console.WriteLine(person3.FirstName); // Salih
+
+Console.WriteLine(((Customer)person3).CreditCardNumber); // 1231546
+
+PersonManager personManager = new PersonManager();
+
+personManager.Add(person3);  // Salih
+personManager.Add(customer); // Salih
+personManager.Add(employee); // Fatih
